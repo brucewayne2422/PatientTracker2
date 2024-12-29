bfb42dcf6310d31b5fb491a33fff180eaba55695
brucewayne2422 <m95uddin@gmail.com>
2024-12-29 18:30:37 +0600

    Fix port binding issue

--- server.js
+++ server.js
@@ -78,5 +78,5 @@ app.delete('/api/patients/:id', async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000; // Get the port from .env or default to 5000
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
