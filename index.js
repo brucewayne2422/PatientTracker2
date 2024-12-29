c47e0b9fccd103d38e39fa4c5735a6abf96485d8
brucewayne2422 <m95uddin@gmail.com>
2024-12-29 18:42:28 +0600

    Fix port binding issue

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-"console.log('Hello, Node.js!');" 
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config(); // Load environment variables from .env
+require('dotenv').config(); // 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const app = express();
+const app = require('./index');
 
 // Middleware
 app.use(cors());
@@ -79,4 +79,4 @@ app.delete('/api/patients/:id', async (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000; 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`);});
